2f45d8af9b9861bc6f6f00e9c91f08d5a6a4de4e
Ange <angenettecastro@yahoo.com>
2018-05-04 14:18:31 +0800

    change form to 3 inputs with average

--- resources/views/backEnd/solderTempCreate.blade.php
+++ resources/views/backEnd/solderTempCreate.blade.php
@@ -20,20 +20,20 @@
                     <div class="form-group">
                         <div class = "row">
                                 <div class = "col-sm-1"> </div>
-                                <div class = "col-sm-1">    {{Form::label('shift','Shift'),['class'=>'form-control']}}  </div>
-                                <div class="col-sm-2">      {{Form::select('shift', array('A' => 'A 6am-2pm', 'B' => 'B 2pm-10pm','C' => 'C 10pm-6am'), 'S',['class'=>'form-control'])}} </div>
-                                <div class = "col-sm-1">    {{Form::label('date','Date'),['class'=>'form-control']}}  </div>
-                                <div class="col-sm-3">      {{Form::date('fixture_date', \Carbon\Carbon::now() ,['class'=>'form-control'] )}}  </div>
-                                <div class = "col-sm-1">    {{Form::label('J-Box','J-Box'),['class'=>'form-control']}}  </div>
+                                <div class = "col-sm-1">   {{Form::label('shift','Shift'),['class'=>'form-control']}}  </div>
+                                <div class="col-sm-2">     {{Form::select('shift', array('A' => 'A 6am-2pm', 'B' => 'B 2pm-10pm','C' => 'C 10pm-6am'), 'S',['class'=>'form-control'])}} </div>
+                                <div class = "col-sm-1">   {{Form::label('date','Date'),['class'=>'form-control']}}  </div>
+                                <div class="col-sm-3">     {{Form::date('fixture_date', \Carbon\Carbon::now() ,['class'=>'form-control'] )}}  </div>
+                                <div class = "col-sm-1">   {{Form::label('J-Box','J-Box'),['class'=>'form-control']}}  </div>
                                 <div class="col-sm-2">     {{ Form::select('jboxName', array('Sunter' => 'Sunter'),'Sunter',['class'=>'form-control'] )}}  </div>
-                                <div class = "col-sm-1"> {{Form::hidden('transID','1'),['class'=>'form-control']}} </div>
+                                <div class = "col-sm-1">   {{Form::hidden('transID','1'),['class'=>'form-control']}} </div>
                       
                         </div>
                         <br/>
                         <div class = "row">
                                 <div class = "col-sm-1"> </div>
                                 <div class = "col-sm-1">     </div>
-                                <div class="col-sm-3">    {{Form::label('tempBefAdj','Temp Before Adjustment'),['class'=>'form-control']}} {{Form::text('tempBefAdj','0' ,['class'=>'form-control'])}} </div>
+                                <div class="col-sm-3">    {{Form::label('tempBefAdj','Temp Before Adjustment'),['class'=>'form-control']}} </div>
                                 <div class = "col-sm-3">   {{Form::label('tempAftAdj','Temp After Adjustment'),['class'=>'form-control']}}  </div>
                                 <div class="col-sm-3">      {{Form::label('average','Average'),['class'=>'form-control']}}</div>
                                 <div class = "col-sm-1"> </div>
@@ -44,18 +44,18 @@
                     <div class = "row">
                                 <div class = "col-sm-1"> </div>
                                 <div class = "col-sm-1">   1  </div>
-                                <div class="col-sm-3">    {{Form::text('tempBefAdj','0' ,['class'=>'form-control'])}}   </div>
-                                <div class = "col-sm-3">   {{Form::text('tempAftAdj','0' ,['class'=>'form-control'])}}   </div>
-                                <div class="col-sm-3">       {{Form::text('tempAvg','0' ,['class'=>'form-control'])}} </div>
+                                <div class="col-sm-3">    {{Form::text('tempBefAdj','Bef1' ,['class'=>'form-control'])}}   </div>
+                                <div class = "col-sm-3">   {{Form::text('tempAftAdj','Aft1' ,['class'=>'form-control'])}}   </div>
+                                <div class="col-sm-3">       {{Form::text('tempAvg','Ave' ,['class'=>'form-control'])}} </div>
                                 <div class = "col-sm-1"> </div>
               
                 </div>
                 <div class = "row">
                                 <div class = "col-sm-1"> </div>
                                 <div class = "col-sm-1">  2   </div>
-                                <div class="col-sm-3">    {{Form::text('tempBefAdj','0' ,['class'=>'form-control'])}}    </div>
-                                <div class = "col-sm-3">   {{Form::text('tempAftAdj','0' ,['class'=>'form-control'])}}   </div>
-                                <div class="col-sm-3">       {{Form::text('tempAvg','0' ,['class'=>'form-control'])}} </div>
+                                <div class="col-sm-3">    {{Form::text('tempBefAdj','Bef2' ,['class'=>'form-control'])}}    </div>
+                                <div class = "col-sm-3">   {{Form::text('tempAftAdj','Aft2' ,['class'=>'form-control'])}}   </div>
+                                <div class="col-sm-3">        </div>
                                 <div class = "col-sm-1"> </div>
               
                 </div>
@@ -64,7 +64,7 @@
                                 <div class = "col-sm-1">  3   </div>
                                 <div class="col-sm-3">    {{Form::text('tempBefAdj','0' ,['class'=>'form-control'])}}  </div>
                                 <div class = "col-sm-3">   {{Form::text('tempAftAdj','0' ,['class'=>'form-control'])}}   </div>
-                                <div class="col-sm-3">       {{Form::text('tempAvg','0' ,['class'=>'form-control'])}} </div>
+                                <div class="col-sm-3">       </div>
                                 <div class = "col-sm-1"> </div>
               
                 </div>
