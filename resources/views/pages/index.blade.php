899a13e213f8eca99b9244bda35a86a8b7c6a765
Jhay Araya <arayajaysons@gmail.com>
2018-05-22 17:13:12 +0800

    Initial Commit

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -3,7 +3,7 @@
         <title>{{config('app.name', 'SOLARPH')}}</title>
 <div class="jumbotron text-center">
         <h1>Welcome to Solar Philippines!</h1>
-        <p>Solar Philippines Module Manufaturing Corporation</p>
+        <p>Solar Philippines Module Manufaturing Corporation!! </p>
         <p><a class="btn btn-primary btn-lg" href="/Summary" role="button">LOGIN</a></p> 
         </div>
 @endsection
