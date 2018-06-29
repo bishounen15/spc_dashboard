35edb4a4ed4a2cda9c28fef5caed59b5a2ba711f
Chris Nevalga <christopher.nevalga@gmail.com>
2018-06-30 07:18:12 +0800

    Changed spc connection

--- config/database.php
+++ config/database.php
@@ -52,7 +52,7 @@ return [
             'driver' => 'mysql',
             'host' => '127.0.0.1',
             'port' => '3306',
-            'database' => 'sp_spc',
+            'database' => 'spc',
             'username' => 'root',
             'password' => '',
             'unix_socket' => '',
