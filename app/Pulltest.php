4244dd8b7387ca3ec8870a649f5003b7e05b7fde
Chris Nevalga <christopher.nevalga@gmail.com>
2018-06-29 16:26:12 +0800

    Change pull test table

--- app/Pulltest.php
+++ app/Pulltest.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Pulltest extends Model
 {
     // Table Name
-    protected $table = 'rtobpull';
+    protected $table = 'pull_tests';
     //Primary Key
     public $primaryKey = 'id';
     //Primary Key
