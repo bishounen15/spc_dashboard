31dd7763924788138c2a514209f030557ce8b95f
Chris Nevalga <christopher.nevalga@gmail.com>
2018-09-13 13:49:07 +0800

    Fixing auth problem, cannot login on web instance

--- config/session.php
+++ config/session.php
@@ -151,7 +151,7 @@ return [
     |
     */
 
-    'domain' => env('SESSION_DOMAIN', "corp.solarphilippines.ph"),
+    'domain' => env('SESSION_DOMAIN', null),
 
     /*
     |--------------------------------------------------------------------------
