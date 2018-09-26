49b78375b713c592009ce4131d773c313792a133
Chris Nevalga <christopher.nevalga@gmail.com>
2018-09-26 15:14:04 +0800

    Bug fixes for initial findings from End User

--- resources/views/proddt/logsheet/list.blade.php
+++ resources/views/proddt/logsheet/list.blade.php
@@ -7,15 +7,16 @@
     <table class="table table-condensed table-striped table-sm" id="logsheet-list" style="width: 100%;">
         <thead class="thead-dark" style="font-size: 0.7em;">
             {{-- <th>#</th> --}}
-            <th width="10%">Date</th>
+            <th width="8%">Date</th>
             <th width="5%">Shift</th>
-            <th width="10%">Station</th>
-            <th width="10%">Time Start</th>
-            <th width="10%">Time End</th>
+            <th width="7%">Station</th>
+            <th width="8%">Time Start</th>
+            <th width="8%">Time End</th>
             <th width="5%">Duration</th>
-            <th width="10%">Issues</th>
+            <th width="14%">Issues</th>
             <th width="10%">Category</th>
-            <th width="15%">Remarks</th>
+            <th width="10%">Remarks</th>
+            <th width="10%">Encoded by</th>
             <th width="15%">Actions</th>
         </thead>
         <tbody class="tbody-light" style="font-size: 0.75em;">
@@ -69,6 +70,7 @@
                 { data: 'issue' },
                 { data: 'category' },
                 { data: 'remarks' },
+                { data: 'user' },
                 { sortable: false, "render": function ( data, type, full, meta ) {
                     return '<div class="row"><div class="col-sm-6"><a href="/proddt/logsheet/'+full.id+'/edit" role="button" class="btn btn-sm btn-success" style="width: 100%;">Edit</a></div>' +
                            '<div class="col-sm-6"><a href="#" data-href="/proddt/logsheet/destroy/'+full.id+'" role="button" class="btn btn-sm btn-danger disabled" data-toggle="modal" data-target="#confirm-delete" id="'+full.description+'" style="width: 100%;">Remove</a></div></div>';
--- routes/web.php
+++ routes/web.php
@@ -145,7 +145,7 @@ Route::post('/link/account','LinkAccountController@link')->name('link_account');
         Route::get('/proddt/setup/downtime/data/{machine_id}/{category_id}', 'DTTypesController@load')->name('dtdowntime_data');
         Route::resource('/proddt/setup/machine/{machine_id}/downtime','DTTypesController');
 
-        Route::post('/proddt/category_list','DTLogSheetsController@listCategories')->name('get_dtcategory_list');
+        Route::post('/proddt/category_id','DTLogSheetsController@getCategory')->name('get_dtcategory');
         Route::post('/proddt/issue_list','DTLogSheetsController@listIssues')->name('get_dtissue_list');
         Route::get('/proddt/logsheet/data', 'DTLogSheetsController@load')->name('logsheet_data');
         Route::get('/proddt/dashboard/data/{date}/{shift}/{station_id}','DTLogSheetsController@dashdata')->name('dt_dashboard_data');
