27a388362b6af5797e324b9ae52349459df667bb
Chris Nevalga <christopher.nevalga@gmail.com>
2018-12-15 07:50:04 +0800

    Add Ticket MVC

---
+++ app/Tickets.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Tickets extends Model
+{
+    //
+    protected $connection = 'tickets';
+    protected $table = 'tabIssue';
+}
---
+++ resources/views/tickets/dashboard.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+@section('content')
+    <h3>Service Tickets Dashboard</h3>
+    <br>
+    <div class="row">
+        @foreach($charts as $chart)
+        <div class="{{count($chart->labels) > 8 ? "col-sm-12" : "col-sm-4"}}">
+            <div class="card">
+                <div class="card-body">
+                    {!! $chart->container() !!}
+                </div>
+            </div>
+        </div>
+        @endforeach
+    </div>
+
+    <script src=//cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.min.js charset=utf-8></script>
+    {{--  <script src=//cdnjs.cloudflare.com/ajax/libs/highcharts/6.0.6/highcharts.js charset=utf-8></script>  --}}
+    @foreach($charts as $chart)
+    {!! $chart->script() !!}
+    @endforeach
+@endsection
+
+@push('jscript')
+<script>
+    
+</script>
+@endpush
