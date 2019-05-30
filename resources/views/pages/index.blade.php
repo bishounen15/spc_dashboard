bfff066efdb6ac81a671e8b7ae7236dc8cdbc04d
Chris Nevalga <christopher.nevalga@gmail.com>
2019-05-30 15:49:45 +0800

    Minor changes on Home Page.

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,15 +1,18 @@
 @extends('layouts.app')
 @section('content')
-        <title>{{config('app.name', 'SOLARPH')}}</title>
-<div class="jumbotron text-center">
-        <h1>Welcome to Solar Philippines!</h1>
-        <p>Solar Philippines Module Manufaturing Corporation</p>
+<div class="container-fluid">
+        <div class="jumbotron text-center">
+        <strong><h1>Welcome to Solar Philippines!</h1></strong>
+        <hr>
+        <h3>Solar Philippines Module <br> Manufaturing Corporation</h3>
         <p>
                 @guest
+                <br>
                 <a class="btn btn-primary btn-lg" href="{{ route('login') }}" role="button">LOGIN</a>
                 @else
-                <a class="btn btn-primary btn-lg" href="{{ route('apps') }}" role="button">GO TO APPS</a>
+                {{-- <a class="btn btn-primary btn-lg" href="{{ route('apps') }}" role="button">GO TO APPS</a> --}}
                 @endguest
         </p> 
         </div>
+</div>
 @endsection
