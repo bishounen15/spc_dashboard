c9ed504b103bd8370a86fdd74df81aef80683d74
Chris Nevalga <christopher.nevalga@gmail.com>
2019-06-05 17:08:45 +0800

    Trina Admin Update: Include Module Grade, EL Grade, and Status in CSV Download

--- resources/views/mes/trina/admin/modupd.blade.php
+++ resources/views/mes/trina/admin/modupd.blade.php
@@ -336,7 +336,7 @@
                                 {
                                     extend:     'csv',
                                     exportOptions: {
-                                        columns: [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ]
+                                        columns: [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 ]
                                     },
                                     text:       'Download CSV',
                                     filename: "modules_csv"
