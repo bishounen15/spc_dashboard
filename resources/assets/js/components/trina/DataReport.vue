fd1407af257ad8af19c60738a6537a5d06c50f5f
Chris Nevalga <christopher.nevalga@gmail.com>
2019-07-19 09:18:55 +0800

    Trina FTD Report: Display product type default when power grade is out of range.

---
+++ resources/assets/js/components/trina/DataReport.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h3 class="mb-4"><i v-bind:class="icon"></i> {{title}}</h3>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+
+        },
+        data() {
+            return {
+
+            }
+        },
+        created() {
+            
+        },
+        props: {
+            title: String,
+            icon: String
+        },
+        methods: {
+
+        }
+    }
+</script>
